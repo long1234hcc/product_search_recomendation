afc450ddb965150688147e3c342b35100c6f1bac
long1234hcc <115656346+long1234hcc@users.noreply.github.com>
2025-02-16 21:34:56 +0700

    Update product_search_recommendation_project.py

--- product_search_recommendation_project.py
+++ product_search_recommendation_project.py
@@ -23,7 +23,7 @@ df = pd.read_excel(file_path)
 df['name'] = df['name'].apply(normalized)
 print(df.shape)
 
-# Khởi tạo embeddings với HuggingFaceEmbeddings từ mô hình SentenceTransformer
+# HuggingFaceEmbeddings SentenceTransformer
 # custom_embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-m3')
 custom_embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large")
 # custom_embeddings = HuggingFaceEmbeddings(model_name='Alibaba-NLP/gte-Qwen2-7B-instruct')
@@ -32,15 +32,13 @@ custom_embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-l
 
 print("Done embedings")
 
-# Tạo danh sách Document với embeddings từ mô hình tùy chỉnh
 documents = []
 for _, row in df.iterrows():
     documents.append(
         Document(
-            page_content=row['name'],          # Nội dung văn bản chính để tạo embeddings
-            metadata={
-                "id": row['id'],               # ID sản phẩm
-                # "category": row['category']    # Ngành hàng của sản phẩm
+            page_content=row['name'],          
+                "id": row['id'],               
+                # "category": row['category']   
             }
         )
     )
@@ -52,10 +50,8 @@ print("Vector_database")
 vector_db = FAISS.from_documents(documents=documents, embedding=custom_embeddings)
 
 print("Read file")
-# Giả sử df_ans đã được đọc từ file Excel và normalized
 df_ans = pd.read_excel(r"/hdd/sv10/svc/docker-svc/jupyter/data/Team_DC/long/Retrival/get_brand_ereport_handle.xlsx", sheet_name="Sheet1")
 
-# Hàm chuẩn hóa chuỗi
 def normalized(text):
     return str(text).lower().strip()
 
@@ -64,34 +60,26 @@ df_ans["product_name_normalized"] = df_ans["product_name_normalized"].apply(norm
 
 
 print("Write json file")
-# Tạo dictionary để lưu kết quả
 dict_direct = {}
 
-# Mở file JSON Lines để lưu dữ liệu
 with open("dict_cate_group.jsonl", "w", encoding="utf-8") as f:
-    # Lặp qua từng dòng trong DataFrame với thanh tiến trình
     for idx, rows in tqdm(df_ans.iterrows(), desc="Process", total=len(df_ans)):
         query = str(rows['product_name_normalized'])
         # category = str(rows['category'])
         
-        # Tạo bộ truy vấn với điểm similarity
         results_with_scores = vector_db.similarity_search_with_score(
             query=query,
             k=5,
             # filter={"category": category}
         )
         
-        # Trích xuất page_content từ kết quả
         page_content_list = [doc.page_content for doc, score in results_with_scores]
         
-        # Nếu page_content_list không tồn tại, gán nó là list trống
         if not page_content_list:
             page_content_list = []
         
-        # Lưu kết quả vào dict_direct với query làm key
         dict_direct[query] = page_content_list
         
-        # Ghi vào file JSON Lines
         json_line = json.dumps({query: page_content_list}, ensure_ascii=False)
         f.write(json_line + "\n")
         
